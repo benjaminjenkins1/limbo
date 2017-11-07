d3a7eb901077506ca142d451a97df5af4d4c74fc
Benjamin <benjamn.jenkins1@marist.edu>
2017-11-07 11:02:34 -0500

    add everything

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Limbo - Home</title>
+    <meta name="author" content="Benjamin Jenkins">
+    <meta name="description" content="Limbo lost and found home page">
+    <meta name="keywords" content="lost and found, limbo, home">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="/css/custom.css" type="text/css">
+    </head>
+    <body>
+    <header>
+        <div class="site-title">
+            <div class="title-limbo"><b>Limbo</b></div>
+            <div class="title-lostandfound"><i>&nbsp;lost & found</i></div>
+        </div>
+        <div class="account-buttons">
+            <button type="button" class="account-button">Placeholder</button>
+        </div>
+    </header>
+    <div class="content-container">
+        <div class="sidebar">
+            <div class="sidebar-login">
+            </div>
+            <div class="sidebar-lost">
+            </div>
+            <div class="sidebar-found">
+            </div>
+        </div>
+        <div class="page-content">
+            <div class="lf-buttons">
+                <button type="button" class="lf-button">I lost something!</button>
+                <button type="button" class="lf-button">I found something!</button>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-author">By Benjamin Jenkins</div>
+    </footer>
+    </body>
+</html>
---
+++ lost.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Limbo - Report a Lost Item</title>
+    <meta name="author" content="Benjamin Jenkins">
+    <meta name="description" content="Limbo lost and found report a lost item">
+    <meta name="keywords" content="lost and found, limbo, lost, report">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="/css/custom.css" type="text/css">
+    </head>
+    <body>
+    <header>
+        <div class="site-title">
+            <div class="title-limbo"><b>Limbo</b></div>
+            <div class="title-lostandfound"><i>&nbsp;lost & found</i></div>
+        </div>
+        <div class="account-buttons">
+            <button type="button" class="account-button">Placeholder</button>
+        </div>
+    </header>
+    <div class="content-container">
+        <div class="sidebar">
+            <div class="sidebar-login">
+            </div>
+            <div class="sidebar-lost">
+            </div>
+            <div class="sidebar-found">
+            </div>
+        </div>
+        <div class="page-content">
+            
+        </div>
+    </div>
+    <footer>
+        <div class="footer-author">By Benjamin Jenkins</div>
+    </footer>
+    </body>
+</html>
